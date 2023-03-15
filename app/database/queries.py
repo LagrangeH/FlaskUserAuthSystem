63f0cbcc741dd6f3d244a25ab8c5b4a59eacb4d0
Maxim Sergeev <maxsergeev39+gh@gmail.com>
2023-03-16 02:40:57 +0300

    Refactor DB usage

--- app/database/queries.py
+++ app/database/queries.py
@@ -1,24 +1,24 @@
 from loguru import logger as log
 
 from .models import User
-from . import get_db
+from . import DB
 
 
 def is_username_registered(username) -> bool:
     # TODO: make case insensitive
-    return get_db().session.query(User.id).filter_by(username=username).scalar() is not None
+    return DB.obj.session.query(User.id).filter_by(username=username).scalar() is not None
 
 
 def is_email_registered(email) -> bool:
     # TODO: make case insensitive
-    return get_db().session.query(User.id).filter_by(email=email).scalar() is not None
+    return DB.obj.session.query(User.id).filter_by(email=email).scalar() is not None
 
 
 def create_user(new_user: User):
-    get_db().session.add(new_user)
-    get_db().session.commit()
+    DB.obj.session.add(new_user)
+    DB.obj.session.commit()
     log.debug(f'{new_user} has been registered!')
 
 
 def get_user_by_email(email) -> User | None:
-    return get_db().session.query(User).filter_by(email=email).first()
+    return DB.obj.session.query(User).filter_by(email=email).first()
