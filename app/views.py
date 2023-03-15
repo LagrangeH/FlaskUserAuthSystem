1f110ad751e82c5689b212e0c6cfd0a7b39ec476
Maxim Sergeev <maxsergeev39+gh@gmail.com>
2023-03-15 05:12:27 +0300

    Add 404 handling

--- app/views.py
+++ app/views.py
@@ -21,6 +21,11 @@ def login_required(view):
     return wrapped_view
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
 @app.route('/')
 def about():
     return render_template('index.html')
