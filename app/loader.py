8bb9fea41e1a6ab44469c863e3b1ffd7e285826c
Maxim Sergeev <maxsergeev39+gh@gmail.com>
2023-03-15 05:00:19 +0300

    Add secret key to Flask config

--- app/loader.py
+++ app/loader.py
@@ -1,3 +1,5 @@
+import secrets
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from loguru import logger as log
@@ -5,6 +7,7 @@ from loguru import logger as log
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///main.db'
+app.secret_key = secrets.token_hex()
 db = SQLAlchemy(app)
 
 
@@ -24,11 +27,4 @@ def configure_logging(debug=False):
 
 def run_app(debug=False):
     configure_logging(debug=debug)
-
-    # Create tables if they don't exist
-    # from db.models import User
-    #
-    # with app.app_context():
-    #     db.create_all()
-
     app.run(debug=True)
