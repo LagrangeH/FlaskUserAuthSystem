3f96d2b6dc5f9f4c2295571ba83b98542f6f5221
Maxim Sergeev <maxsergeev39+gh@gmail.com>
2023-03-16 05:46:52 +0300

    Enable CSRF protection

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ import secrets
 
 from flask import Flask
 from flask_login import LoginManager
+from flask_wtf import CSRFProtect
 from loguru import logger as log
 
 from database import DB
@@ -18,17 +19,22 @@ def create_app(debug: bool = False) -> Flask:
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///main.db'
     app.config['RECAPTCHA_PUBLIC_KEY'] = "6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI"
     app.config['RECAPTCHA_PRIVATE_KEY'] = "6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe"
+    app.config['WTF_CSRF_SECRET_KEY'] = secrets.token_hex()
     app.secret_key = secrets.token_hex()
     app.debug = debug
 
-    # Database initialization
+    # Pushing app context
     app.app_context().push()
+
+    # Database initialization
     DB.obj.init_app(app)
 
     # Login manager initialization
-    login_manager = LoginManager()
+    login_manager = LoginManager(app)
     login_manager.login_view = 'signin'
-    login_manager.init_app(app)
+
+    # CSRF protection initialization
+    csrf = CSRFProtect(app)
 
     @login_manager.user_loader
     def load_user(user_id: int) -> User | None:
@@ -42,8 +48,7 @@ def create_app(debug: bool = False) -> Flask:
 
     # Database creation
     from database import models
-    with app.app_context():
-        DB.obj.create_all()
+    DB.obj.create_all()
 
     log.debug(f"App created with debug={debug}")
     return app
--- app/blueprints/main.py
+++ app/blueprints/main.py
@@ -1,12 +1,19 @@
 from flask import Blueprint, render_template
 from flask_login import current_user
+from flask_wtf.csrf import CSRFError
+
 
 bp = Blueprint('main', __name__, url_prefix='/')
 
 
 @bp.errorhandler(404)
 def page_not_found():
-    return render_template('404.html'), 404
+    return render_template('errors/404.html'), 404
+
+
+@bp.errorhandler(CSRFError)
+def handle_csrf_error(e):
+    return render_template('errors/csrf_error.html', reason=e.description), 400
 
 
 @bp.route('/')
