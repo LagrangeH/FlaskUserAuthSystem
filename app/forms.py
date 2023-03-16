e21cc45a784e340eb7790026d5391205e66c25e0
Maxim Sergeev <maxsergeev39+gh@gmail.com>
2023-03-16 05:53:21 +0300

    Add other forms

--- app/forms.py
+++ app/forms.py
@@ -3,10 +3,27 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
 
-class SignUpForm(FlaskForm):
+class _BaseForm(FlaskForm):
+    recaptcha = RecaptchaField()
+    submit = SubmitField('submit')
+
+
+class SignUpForm(_BaseForm):
     username = StringField('username', validators=[DataRequired()])
     email = StringField('email', validators=[DataRequired()])
     password = StringField('password', validators=[DataRequired()])
     confirm_password = StringField('confirm_password', validators=[DataRequired()])
-    recaptcha = RecaptchaField()
-    submit = SubmitField('submit')
+
+
+class SignInForm(_BaseForm):
+    email = StringField('email', validators=[DataRequired()])
+    password = StringField('password', validators=[DataRequired()])
+
+
+class ResetPasswordForm(_BaseForm):
+    email = StringField('email', validators=[DataRequired()])
+
+
+class RestorePasswordForm(_BaseForm):
+    password = StringField('password', validators=[DataRequired()])
+    confirm_password = StringField('confirm_password', validators=[DataRequired()])
