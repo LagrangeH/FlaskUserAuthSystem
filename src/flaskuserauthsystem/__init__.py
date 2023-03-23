53e926014951ee52c50028ee41a146837342c2f7
Maxim Sergeev <maxsergeev39+gh@gmail.com>
2023-03-23 04:35:30 +0300

    Fix error with db creation

--- src/flaskuserauthsystem/__init__.py
+++ src/flaskuserauthsystem/__init__.py
@@ -33,6 +33,9 @@ def create_app() -> Flask:
 
     login_manager.login_view = 'auth.signin'
 
+    with app.app_context():
+        db.create_all()
+
     from src.flaskuserauthsystem.blueprints import main, auth, errors
     app.register_blueprint(errors.bp)
     app.register_blueprint(main.bp)
