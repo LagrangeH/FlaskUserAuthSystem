912a332501b57d6c79eecc39f2b80a0a53d66c6f
Maxim Sergeev <maxsergeev39+gh@gmail.com>
2023-03-23 03:28:26 +0300

    Add more logging configs

--- src/flaskuserauthsystem/log_config.py
+++ src/flaskuserauthsystem/log_config.py
@@ -1,9 +1,37 @@
+import sys
 from datetime import datetime
 
 from loguru import logger as log
 
 
 def configure_logging(debug: bool = False) -> None:
+    log.remove()
+
+    log.add(
+        sys.stdout,
+        level='DEBUG' if debug else 'INFO',
+        colorize=True,
+        backtrace=debug,
+        diagnose=debug,
+        enqueue=True,
+        catch=True,
+    )
+
+    log.add(
+        f'logs/error_{datetime.utcnow()}.log',
+        level='ERROR',
+        colorize=False,
+        backtrace=debug,
+        diagnose=debug,
+        enqueue=True,
+        catch=True,
+        delay=True,
+        rotation='100 MB',
+        retention='1 months',
+        compression='zip',
+
+    )
+
     if debug:
         log.add(
             f'logs/debug_{datetime.utcnow()}.log',
