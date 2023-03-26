125e7ed0e9f9abb59cd99afa59658161e8048d90
Maxim Sergeev <maxsergeev39+gh@gmail.com>
2023-03-26 05:33:58 +0300

    Bugfix config

--- src/flaskuserauthsystem/config/app_config.py
+++ src/flaskuserauthsystem/config/app_config.py
@@ -5,6 +5,8 @@ from src.flaskuserauthsystem.config import env
 
 DEBUG: bool = env.bool("FLASK_DEBUG", default=False)
 SECRET_KEY: str = env.str("SECRET_KEY", default=secrets.token_hex())
+RECAPTCHA_PUBLIC_KEY: str = env.str("RECAPTCHA_PUBLIC_KEY")
+RECAPTCHA_PRIVATE_KEY: str = env.str("RECAPTCHA_PRIVATE_KEY")
 WTF_CSRF_SECRET_KEY: str = env.str("WTF_CSRF_SECRET_KEY", default=secrets.token_hex())
 SQLALCHEMY_DATABASE_URI: str = env.str(
     "SQLALCHEMY_DATABASE_URI",
