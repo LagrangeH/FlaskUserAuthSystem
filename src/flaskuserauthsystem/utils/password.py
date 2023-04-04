229bb05466011f7770d3991a23d70ab491dabe6a
Maxim Sergeev <maxsergeev39+gh@gmail.com>
2023-04-04 04:15:41 +0300

    Add annotations

--- src/flaskuserauthsystem/utils/password.py
+++ src/flaskuserauthsystem/utils/password.py
@@ -1,11 +1,11 @@
 import bcrypt
 
 
-def hash_password(password):
+def hash_password(password: str | bytes) -> bytes:
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
 
-def check_password(password, password_hash):
+def check_password(password: str | bytes, password_hash: str | bytes) -> bool:
     return bcrypt.checkpw(
         password.encode('utf-8'),
         password_hash.encode('utf-8')
